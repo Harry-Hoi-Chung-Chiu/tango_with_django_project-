d87500cf5a0b9d5933d9d0b877764d1bc8abbf10
Hoi Chung Chiu <2679319C@student.gla.ac.uk>
2023-02-04 06:19:56 +0000

    Added populate()

--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
     def __str__(self):
         return self.name
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
