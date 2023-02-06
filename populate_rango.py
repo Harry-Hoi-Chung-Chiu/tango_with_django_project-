755564efda126523f3992d6f225483170fed5b81
Hoi Chung Chiu <2679319C@student.gla.ac.uk>
2023-02-06 03:01:02 +0000

    added most liked categories and most viewed pages

--- populate_rango.py
+++ populate_rango.py
@@ -27,27 +27,27 @@ def add_cat(name, views=0, likes=0):
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
+         'url': 'http://docs.python.org/3/tutorial/','views':26},
         {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/','views':42},
         {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}
+         'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 52}
     ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':36},
         {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
+         'url': 'http://www.djangorocks.com/', 'views':72},
         {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}
+         'url': 'http://www.tangowithdjango.com/', 'views':92}
     ]
 
     other_pages = [
         {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
+         'url': 'http://bottlepy.org/docs/dev/', 'views':52},
         {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}
+         'url': 'http://flask.pocoo.org', 'views':60}
     ]
 
     cats = {
@@ -58,7 +58,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cats[cat]['views'], cats[cat]['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
