061bfb8a571adcae9fea21a33a88bd588e4b959a
Hoi Chung Chiu <2679319C@student.gla.ac.uk>
2023-02-07 04:13:20 +0000

    added index link in categories page

--- rango/views.py
+++ rango/views.py
@@ -25,6 +25,7 @@ def show_category(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
         context_dict['category'] = category
+        context_dict['pages'] = pages
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
