c7ae608239595f5b30c7ce8ee3075e23f8c5381e
Hoi Chung Chiu <2679319C@student.gla.ac.uk>
2023-02-10 09:17:08 +0000

    Added cookies and sessions. Should work for both chapter 9 and 10 automated testing

--- rango/views.py
+++ rango/views.py
@@ -6,8 +6,24 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val=default_val
+    return val
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits','1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if(datetime.now()-last_visit_time).days >0:
+        visits+=1
+        request.session['last_visit']= str(datetime.now())
+    else:
+        request.session['last_visit']= last_visit_cookie
+    request.session['visits']=visits
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -16,11 +32,17 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    response= render(request, 'rango/index.html', context=context_dict)
+
+    return response
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    visitor_cookie_handler(request)
+    context_dict={}
+    context_dict['visits']=request.session['visits']
+    return render(request, 'rango/about.html',context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -46,7 +68,7 @@ def add_category(request):
         if form.is_valid():
             cat = form.save(commit=True)
             print(cat, cat.slug)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -58,7 +80,7 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category=None
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     form = PageForm()
     if request.method=='POST':
         form = PageForm(request.POST)
